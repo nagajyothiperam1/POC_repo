e03d382403465ba64d88ac418172a116f65565ac
Peram Naga Jyothi <39182902+nagajyothiperam1@users.noreply.github.com>
2019-07-31 16:50:41 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,4 +1,5 @@
+execute(){
 javac CreateAJar.java
 jar -cvf MyJar.jar CreateAJar.class
-mkdir \\3.18.218.105\root\jars\master
-cp *.jar \\3.18.218.105\root\jars\master
+cp *.jar \\3.18.218.105\root\jars
+}
