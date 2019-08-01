c579f90d658bf138ee32f1aa6a44e31ee012da23
Peram Naga Jyothi <39182902+nagajyothiperam1@users.noreply.github.com>
2019-08-01 16:04:47 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,5 +4,6 @@ pwd
 cd ~
 cd ..
 find . -name "doc.pem"
-scp -i doc.pem /home/jenkins/workspace/POC_repo_master/*.jar ec2-user@ec2-3-18-218-105.us-east-2.compute.amazonaws.com:/home
+ssh -i doc.pem ec2-user@ec2-3-18-218-105.us-east-2.compute.amazonaws.com && "sudo ls /root"
+
 '''
