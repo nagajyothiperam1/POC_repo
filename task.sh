bc65f17e13c450601b5428afe614024b32144d50
Peram Naga Jyothi <39182902+nagajyothiperam1@users.noreply.github.com>
2019-08-01 18:42:36 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,6 +4,7 @@ pwd
 cd ~
 cd ..
 find . -name "doc.pem"
+ls
 chmod 400 doc.pem
-scp -i doc.pem *.jar ec2-user@3.18.218.105:~/
+scp -i doc.pem /home/jenkins/workspace/POC_repo_master/*.jar ec2-user@3.18.218.105:~/
 '''
